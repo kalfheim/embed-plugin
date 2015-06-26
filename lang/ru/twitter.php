538525074a993f98a769557bb712339a0f6418e1
exotickg1 <exotickg1@gmail.com>
2015-06-26 16:15:44 +0600

    Create twitter.php

---
+++ lang/ru/twitter.php
@@ -0,0 +1,29 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| Language File For Krisawzm.Embed Plugin
+|--------------------------------------------------------------------------
+|
+| If you translate this to a different language,
+| please share it on Github!
+|
+| https://github.com/krisawzm/embed-plugin
+|
+*/
+return [
+    'details' => [
+        'name'                  => 'Twitter',
+        'description'           => 'Вставляет твит с Twitter.',
+    ],
+    'properties' => [
+        'href' => [
+            'title'             => 'Твит',
+            'description'       => 'Нажмите на Твит, затем скопируйте URL с адресной строки..',
+            'validationMessage' => 'Неправильная ссылка на Твит.',
+        ],
+        'hideMedia' => [
+            'title'             => 'Спрятать изображение и видео',
+            'description'       => 'Отключает отображение изображений и видео.',
+        ],
+    ],
+];
