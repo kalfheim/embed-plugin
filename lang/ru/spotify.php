a710c7bc7842f71760df86a763a93b8615b4ae8b
exotickg1 <exotickg1@gmail.com>
2015-06-26 16:11:58 +0600

    Create spotify.php

---
+++ lang/ru/spotify.php
@@ -0,0 +1,25 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| Language File For Krisawzm.Embed Plugin
+|--------------------------------------------------------------------------
+|
+| If you translate this to a different language,
+| please share it on Github!
+|
+| https://github.com/krisawzm/embed-plugin
+|
+*/
+return [
+    'details' => [
+        'name'                  => 'Spotify',
+        'description'           => 'Вставляет кнопку Play с Spotify.',
+    ],
+    'properties' => [
+        'id' => [
+            'title'             => 'Трек',
+            'description'       => 'Ссылка Spotify. Правой кнопкой мыши на трек > Скопировать Spotify URI',
+            'validationMessage' => 'Неправильная ссылка.',
+        ],
+    ],
+];
