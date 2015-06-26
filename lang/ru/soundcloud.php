11a41229eb4890c3bf2092c0242e1e333275b516
exotickg1 <exotickg1@gmail.com>
2015-06-26 16:10:15 +0600

    Create soundcloud.php

---
+++ lang/ru/soundcloud.php
@@ -0,0 +1,29 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| Language File For Krisawzm.Embed Plugin
+|--------------------------------------------------------------------------
+|
+| If you translate this to a different language,
+| please share it on Github!
+|
+| https://github.com/krisawzm/embed-plugin
+|
+*/
+return [
+    'details' => [
+        'name'                  => 'SoundCloud',
+        'description'           => 'Вставляет трек или плейлист с SoundCloud.',
+    ],
+    'properties' => [
+        'url' => [
+            'title'             => 'Sound URL',
+            'description'       => 'Полный URL. Работает с синглами, профилями, альбомами и плейлистами.',
+            'validationMessage' => 'URL неправильный.',
+        ],
+        'autoPlay' => [
+            'title'             => 'Автовоспроизведение',
+            'description'       => 'Воспроизводить автоматически, когда загружается страница.',
+        ],
+    ],
+];
