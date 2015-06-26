4c707bf3f435ffa4314babc1dc5a5b9898c9502c
exotickg1 <exotickg1@gmail.com>
2015-06-26 15:40:12 +0600

    Create flickr.php

---
+++ lang/ru/flickr.php
@@ -0,0 +1,25 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| Language File For Krisawzm.Embed Plugin
+|--------------------------------------------------------------------------
+|
+| If you translate this to a different language,
+| please share it on Github!
+|
+| https://github.com/krisawzm/embed-plugin
+|
+*/
+return [
+    'details' => [
+        'name'                  => 'Flickr',
+        'description'           => 'Вставляет картинку или альбом Flickr.',
+    ],
+    'properties' => [
+        'url' => [
+            'title'             => 'URL',
+            'description'       => 'Полный URL Flickr галереи.',
+            'validationMessage' => 'Неправильный URL.',
+        ],
+    ],
+];
