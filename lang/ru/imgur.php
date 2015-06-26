6dce59f6dbfd873a3aed9e8ecdb788e125a11014
exotickg1 <exotickg1@gmail.com>
2015-06-26 15:48:11 +0600

    Create imgur.php

---
+++ lang/ru/imgur.php
@@ -0,0 +1,29 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| Language File For Krisawzm.Embed Plugin
+|--------------------------------------------------------------------------
+|
+| If you translate this to a different language,
+| please share it on Github!
+|
+| https://github.com/krisawzm/embed-plugin
+|
+*/
+return [
+    'details' => [
+        'name'                  => 'Imgur',
+        'description'           => 'Вставляет картинки или альбомы с Imgur.',
+    ],
+    'properties' => [
+        'id' => [
+            'title'             => 'Пост',
+            'description'       => 'ID поста или URL. Так же работает с альбомами.',
+            'validationMessage' => 'Неправильная ссылка.',
+        ],
+       'hideDetails' => [
+            'title'             => 'Скрыть подробности Поста',
+            'description'       => 'Скрыть метаданные: имя пользователя, просмотры и количество комментариев.',
+        ],
+    ],
+];
