3ae59e684d17569089175df2b6eacfb2cfc05494
exotickg1 <exotickg1@gmail.com>
2015-06-26 15:33:17 +0600

    Create codepen.php

---
+++ lang/ru/codepen.php
@@ -0,0 +1,29 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| Language File For Krisawzm.Embed Plugin
+|--------------------------------------------------------------------------
+|
+| If you translate this to a different language,
+| please share it on Github!
+|
+| https://github.com/krisawzm/embed-plugin
+|
+*/
+return [
+    'details' => [
+        'name'                  => 'CodePen',
+        'description'           => 'Вставляет Pen'ы с CodePen.',
+    ],
+    'properties' => [
+        'id' => [
+            'title'             => 'Pen',
+            'description'       => 'ID или полный URL вашего Pen, который вы хотите отобразить. Например: http://codepen.io/user/pen/id',
+            'validationMessage' => 'Неправильный Pen ID.',
+        ],
+        'defaultTab' => [
+            'title'             => 'Стандартная вкладка',
+            'description'       => 'Выберите, какую вкладку отображать сначала.',
+        ],
+    ],
+];
