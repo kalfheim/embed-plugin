985aec1abd42f8aaed9041d32175a8e564154fcc
exotickg1 <exotickg1@gmail.com>
2015-06-26 15:54:09 +0600

    Create jsfiddle.php

---
+++ lang/ru/jsfiddle.php
@@ -0,0 +1,36 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| Language File For Krisawzm.Embed Plugin
+|--------------------------------------------------------------------------
+|
+| If you translate this to a different language,
+| please share it on Github!
+|
+| https://github.com/krisawzm/embed-plugin
+|
+*/
+return [
+    'details' => [
+        'name'                  => 'jsFiddle',
+        'description'           => 'Вставляет jsFiddle.',
+    ],
+    'properties' => [
+        'id' => [
+            'title'             => 'jsFiddle',
+            'description'       => 'ID или полный URL фиддла, который вы хотите отобразить. Например: https://jsfiddle.net/id/',
+            'validationMessage' => 'Неправильный фиддл ID.',
+        ],
+        'tabs' => [
+            'title'             => 'Вкладки',
+            'description'       => 'Удалить или изменить вкладки. По умолчанию: "js,resources,html,css,result"',
+            'validationMessage' => 'Неправильные вкладки.',
+        ],
+       'skin' => [
+            'title'             => 'Стиль',
+            'description'       => 'Выберите стиль, который необходимо использовать.',
+            'light'             => 'Light',
+            'presentation'      => 'Presentation',
+        ],
+    ],
+];
